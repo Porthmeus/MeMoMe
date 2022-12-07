37ba22fbb96c507ec31c185a26087f02a7032e4e
Thomas Dost <stu203329@mail.uni-kiel.de>
2022-12-07 19:54:40 +0100

    Refactored testcase

--- tests/test_download.py
+++ tests/test_download.py
@@ -5,11 +5,12 @@ from src import download_db
 
 
 class TestDownload(unittest.TestCase):
+    this_path = Path(__file__)
     def test_non_ex_parent(self):
         # get current path
-        this_path = Path(__file__)
+
         # create a nonexistent target to download the stuff to
-        non_exist_path = this_path.parent.joinpath(Path("nonexist"))
+        non_exist_path = self.this_path.parent.joinpath(Path("nonexist"))
         # The call to creat_folder should return Path("")
         self.assertEqual(download_db.create_folder(non_exist_path), Path(""))
 
