dedf050b0e8bbd19bbc44bf655445bc3b0c6607a
Porthmeus <jan.taubenheim@web.de>
2023-05-02 09:43:35 +0200

    changed the matchInchi file to matchInchiRoutines, but added a new function 'matchInchi' which takes care of the InChI matching (including differences in charges). Still needs a test and some thorough testing with different molecules

--- src/matchInchi.py
+++ src/matchInchi.py
@@ -1,57 +1,34 @@
 # Porthmeus
-# 15.03.23
+# 27.04.23
 
-# Some functions to compare InChi keys/strings and tell if it is the same chemical structure
+from src.matchInchiRoutines import *
+from rdkit import Chem
 
-from rdkit import Chem,DataStructs
-from src.downloadPubchemInfo import getPubchemInfo
+def matchInchi(inchi1:str, inchi2:str) -> tuple:
+    ''' A combination of different matching algorithms for the Inchi-Strings, which end in a simple yes/no answer, whether the molecules are the same'''
 
+    # check whether the molecules are differently charged, if so neutralize the charges
+    m1 = Chem.MolFromInchi(inchi1)
+    m2 = Chem.MolFromInchi(inchi2)
+    charge1 = Chem.GetFormalCharge(m1)
+    charge2 = Chem.GetFormalCharge(m2)
+    if charge1 != charge2:
+        m1 = NeutraliseCharges(m1)
+        m2 = NeutraliseCharges(m2)
+        inchi1 = Chem.MolToInchi(m1)
+        inchi2 = Chem.MolToInchi(m2)
+    chargeDiff = charge1 - charge2
 
+    # check the fingerprints - they should be the same
+    if compareInchiByFingerprint(inchi1,inchi2) != 1.0:
+        return(False,chargeDiff)
 
-def compareInchiByFingerprint(met1:str, met2:str, method:str = "Dice") -> float:
-    ''' Simple function to compare based on fingerprints
-    @met1 - str of the first inchi string
-    @met2 - str of the second inchi string
-    @method - str defining the method to use. One of 'Tanimoto', 'Dice', 'Cosine', 'Sokal', 'Russel', 'Kulczynski', 'McConnaughey'.'''
-    
-    # correct and check method string if implemented in RDKit
-    method = method[0].upper() + method[1:].lower()
-    impl_methods = ['Tanimoto', 'Dice', 'Cosine', 'Sokal', 'Russel', 'Kulczynski', 'McConnaughey'] # "Tversky" is noted in the documentation, but throws an error
-    if method not in impl_methods:
-        raise ValueError(method+ " not implemented. Choose one of: " + ", ".join(impl_methods))
-
-    # do the conversion
-    m1 = Chem.MolFromInchi(met1)
-    m2 = Chem.MolFromInchi(met2)
-    fp1 = Chem.RDKFingerprint(m1)
-    fp2 = Chem.RDKFingerprint(m2)
-    sim = DataStructs.FingerprintSimilarity(fp1,fp2, metric = eval("DataStructs."+method+"Similarity"))
-    return(sim)
-
-def compareInchiByCSmile(met1:str, met2:str) -> bool:
-    '''Converts Inchi to canonical smile and compares the strings'''
-    # do the conversion
-    m1 = Chem.MolFromInchi(met1)
-    m2 = Chem.MolFromInchi(met2)
-    sm1 = Chem.MolToSmiles(m1)
-    sm2 = Chem.MolToSmiles(m2)
-    return(sm1 == sm2)
+    # checking the canonical InchiString
+    same = compareInchiByString(inchi1, inchi2)
+    if same == True:
+        return(same, chargeDiff)
 
 
-def compareInchiByKey(met1:str, met2:str) -> bool:
-    '''Converts Inchi to Inchi-key and compares the hashes'''
-    # do the conversion
-    m1 = Chem.MolFromInchi(met1)
-    m2 = Chem.MolFromInchi(met2)
-    key1 = Chem.MolToInchiKey(m1)
-    key2 = Chem.MolToInchiKey(m2)
-    return(key1 == key2)
-
-def compareInchiByString(met1:str, met2:str) -> bool:
-    '''Converts Inchi to canonical Inchi and compares the strings'''
-    # do the conversion
-    m1 = Chem.MolFromInchi(met1)
-    m2 = Chem.MolFromInchi(met2)
-    key1 = Chem.MolToInchi(m1)
-    key2 = Chem.MolToInchi(m2)
-    return(key1 == key2)
+    # if that is false check if there is one of the stereoisomers the same
+    same = compareInchiByStereoIsomer(inchi1,inchi2)
+    return(same, chargeDiff)
---
+++ src/matchInchiRoutines.py
@@ -0,0 +1,118 @@
+# Porthmeus
+# 15.03.23
+
+# Some functions to compare InChi keys/strings and tell if it is the same chemical structure
+
+from rdkit import Chem,DataStructs
+from src.downloadPubchemInfo import getPubchemInfo
+from rdkit.Chem.EnumerateStereoisomers import EnumerateStereoisomers, StereoEnumerationOptions
+from rdkit.Chem import AllChem
+from rdkit.Chem.rdchem import Mol
+import rdkit
+
+
+def compareInchiByFingerprint(met1:str, met2:str, method:str = "Dice") -> float:
+    ''' Simple function to compare based on fingerprints
+    @met1 - str of the first inchi string
+    @met2 - str of the second inchi string
+    @method - str defining the method to use. One of 'Tanimoto', 'Dice', 'Cosine', 'Sokal', 'Russel', 'Kulczynski', 'McConnaughey'.'''
+    
+    # correct and check method string if implemented in RDKit
+    #method = method[0].upper() + method[1:].lower()
+    impl_methods = ['Tanimoto', 'Dice', 'Cosine', 'Sokal', 'Russel', 'Kulczynski', 'McConnaughey'] # "Tversky" is noted in the documentation, but throws an error
+    if method not in impl_methods:
+        raise ValueError(method+ " not implemented. Choose one of: " + ", ".join(impl_methods))
+
+    # do the conversion
+    m1 = Chem.MolFromInchi(met1)
+    m2 = Chem.MolFromInchi(met2)
+    fp1 = Chem.RDKFingerprint(m1)
+    fp2 = Chem.RDKFingerprint(m2)
+    sim = DataStructs.FingerprintSimilarity(fp1,fp2, metric = eval("DataStructs."+method+"Similarity"))
+    return(sim)
+
+def compareInchiByCSmile(met1:str, met2:str) -> bool:
+    '''Converts Inchi to canonical smile and compares the strings'''
+    # do the conversion
+    m1 = Chem.MolFromInchi(met1)
+    m2 = Chem.MolFromInchi(met2)
+    sm1 = Chem.MolToSmiles(m1)
+    sm2 = Chem.MolToSmiles(m2)
+    return(sm1 == sm2)
+
+
+def compareInchiByKey(met1:str, met2:str) -> bool:
+    '''Converts Inchi to Inchi-key and compares the hashes'''
+    # do the conversion
+    m1 = Chem.MolFromInchi(met1)
+    m2 = Chem.MolFromInchi(met2)
+    key1 = Chem.MolToInchiKey(m1)
+    key2 = Chem.MolToInchiKey(m2)
+    return(key1 == key2)
+
+def compareInchiByString(met1:str, met2:str) -> bool:
+    '''Converts Inchi to canonical Inchi and compares the strings'''
+    # do the conversion
+    m1 = Chem.MolFromInchi(met1)
+    m2 = Chem.MolFromInchi(met2)
+    key1 = Chem.MolToInchi(m1)
+    key2 = Chem.MolToInchi(m2)
+    return(key1 == key2)
+
+def compareInchiByStringNoHydrogen(met1:str, met2:str) -> bool:
+    '''Converts Inchi to canonical InChI after removal of Hydrogens'''
+    # do conversion
+    m1 = Chem.MolFromInchi(met1)
+    m2 = Chem.MolFromInchi(met2)
+    # remove Hs
+    m1 = Chem.RemoveAllHs(m1)
+    m2 = Chem.RemoveAllHs(m2)
+    # compare string
+    key1 = Chem.MolToInchi(m1)
+    key2 = Chem.MolToInchi(m2)
+    return(key1 == key2)
+
+def compareInchiByStereoIsomer(met1:str, met2:str) -> bool:
+    '''Enumerates all stereoisomers of both metabolites and checks if there is a common set between both metabolites'''
+    # do conversion
+    m1 = Chem.MolFromInchi(met1)
+    m2 = Chem.MolFromInchi(met2)
+    # find also stereoisomers in defined InChI
+    opts = StereoEnumerationOptions(onlyUnassigned=False)
+    stereo1 = [Chem.MolToInchi(x) for x in EnumerateStereoisomers(m1, options = opts)]
+    stereo2 = [Chem.MolToInchi(x) for x in EnumerateStereoisomers(m2, options = opts)]
+    intersect = list(set(stereo1).intersection(stereo2))
+    return(len(intersect) > 0)
+
+def NeutraliseCharges(mol:rdkit.Chem.rdchem.Mol) -> rdkit.Chem.rdchem.Mol:
+    ''' Takes a molecule and returns the neutralized version of it + an indicator, what the charge difference was.'''
+
+    #""" adapted from Hans de Winter - https://rdkit.readthedocs.io/en/latest/Cookbook.html """
+    # initialize the patterns
+    patts= (
+        # Imidazoles
+        ('[n+;H]','n'),
+        # Amines
+        ('[N+;!H0]','N'),
+        # Carboxylic acids and alcohols
+        ('[$([O-]);!$([O-][#7])]','O'),
+        # Thiols
+        ('[S-;X1]','S'),
+        # Sulfonamides
+        ('[$([N-;X2]S(=O)=O)]','N'),
+        # Enamines
+        ('[$([N-;X2][C,N]=C)]','N'),
+        # Tetrazoles
+        ('[n-]','[nH]'),
+        # Sulfoxides
+        ('[$([S-]=O)]','S'),
+        # Amides
+        ('[$([N-]C=O)]','N'),
+        )
+    reactions = [(Chem.MolFromSmarts(x),Chem.MolFromSmiles(y,False)) for x,y in patts]
+
+    for i,(reactant, product) in enumerate(reactions):
+        while mol.HasSubstructMatch(reactant):
+            rms = AllChem.ReplaceSubstructs(mol, reactant, product)
+            mol = rms[0]
+    return(mol)
