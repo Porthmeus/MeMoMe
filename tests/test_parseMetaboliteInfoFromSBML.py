7af2b438e575099e567e9990a0894579affdfbc2
Porthmeus <jan.taubenheim@web.de>
2023-06-13 06:35:42 +0200

    updated tests to fit the new structure of metabolites being objects

--- tests/test_parseMetaboliteInfoFromSBML.py
+++ tests/test_parseMetaboliteInfoFromSBML.py
@@ -8,6 +8,7 @@ from unittest import TestCase
 import pandas as pd
 
 from src.parseMetaboliteInfoFromSBML import *
+from src.nameHandling import *
 
 
 class TestParseSBML(unittest.TestCase):
@@ -23,8 +24,12 @@ class TestParseSBML(unittest.TestCase):
         
         # parse the model
         ecoli_core_sbml = self.dat.joinpath("e_coli_core.xml")
-        test_df = parseMetaboliteInfoFromSBML(ecoli_core_sbml)
-        self.assertTrue(all(test_df == expected_df))
+        test_mets = parseMetaboliteInfoFromSBML(ecoli_core_sbml)
+
+        # do some tests
+        ids = pd.unique([removeMetabolitePrefixSuffix(x) for x in expected_df.loc[:,"ID"]])
+        self.assertTrue(all([x._id in ids for x in test_mets]))
+        # TODO add more tests here
     
     def test_SBMLfileNonExistant(self):
         # test whether the function still handles non existant files, as libSBML will not complain about that by default
@@ -38,6 +43,13 @@ class TestParseSBML(unittest.TestCase):
         teststr = "M_cPd00001_c0"
         self.assertEqual(extract_cpd(teststr), None)
 
+    def test_removeMetabolitePrefixSuffix(self):
+        teststr = "M_cpd00001_c0"
+        self.assertEqual(removeMetabolitePrefixSuffix(teststr), "cpd00001")
+        teststr = "M_glc__D_c"
+        self.assertEqual(removeMetabolitePrefixSuffix(teststr), "glc__D")
+        teststr = "M_glc__D_e"
+        self.assertEqual(removeMetabolitePrefixSuffix(teststr), "glc__D")
 
 if __name__ == '__main__':
     unittest.main()
