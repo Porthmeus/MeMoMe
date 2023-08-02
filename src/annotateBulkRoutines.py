9b02400e327148364d9a862aa73a9f304d2e374f
Thomas Dost <stu203329@mail.uni-kiel.de>
2023-08-02 12:55:54 +0200

    Revert "Fixes #44"
    This reverts commit 8036b999081487e828ab3bdf0de610ad964d8499.

--- src/annotateBulkRoutines.py
+++ src/annotateBulkRoutines.py
@@ -4,7 +4,6 @@
 '''
 This file contains function to annotated MeMoMetabolites in a bulk manner. This means a list of MeMoMetabolites is parsed and the annotation takes place on the metabolites in that list. This is mainly to use within the MeMoModel.annotate() function
 '''
-from typing import Tuple
 
 import numpy as np
 import pandas as pd
@@ -13,20 +12,16 @@ from src.MeMoMetabolite import MeMoMetabolite
 from src.annotateInchiRoutines import findOptimalInchi
 
 
-def annotateChEBI(metabolites: list[MeMoMetabolite]) -> Tuple[int, int]:
+def annotateChEBI(metabolites: list[MeMoMetabolite]) -> None:
     """ Annotate the metaboltes with Inchis from ChEBI """
 
     # check if any unannotated metabolites exist 
     ids = [x for x, y in enumerate(metabolites) if y._inchi_string == None]
-    not_annotated_metabolites = len(ids)
-    annotated_by_chebi = 0
     # check if chebis ids are actually present in the annotation slot
     annos = any(["chebi" in x.annotations.keys() for i, x in enumerate(metabolites) if i in ids])
     if annos:
         # download the information from the server
-        print("TEST")
         chebi_db = pd.read_table("https://ftp.ebi.ac.uk/pub/databases/chebi/Flat_file_tab_delimited/chebiId_inchi.tsv")
-        print("TEST2")
         chebi_db.index = chebi_db['CHEBI_ID']
 
         # annotate the metabolites with the inchi_string
@@ -38,7 +33,6 @@ def annotateChEBI(metabolites: list[MeMoMetabolite]) -> Tuple[int, int]:
             inchis = np.unique(chebi_db.loc[chebis, "InChI"])
             if len(inchis) > 0:
                 inchi = findOptimalInchi(inchis)
-                annotated_by_chebi += 2
             metabolites[i].set_inchi_string(inchi)
 
-    return annotated_by_chebi, not_annotated_metabolites
+    return (None)
