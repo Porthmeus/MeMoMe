294e0993f731392d1964596f57395972bb44968c
Thomas Dost <stu203329@mail.uni-kiel.de>
2023-08-02 11:31:23 +0200

    Added paraments for the first and second model

--- main.py
+++ main.py
@@ -4,13 +4,31 @@
 Main entry point of the program
 """
 import argparse
+import logging
+
 from src.download_db import download
+
+# Configure the logger
+logging.basicConfig(
+    level=logging.DEBUG,  # Set the logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+# Create a logger with the desired name
+logger = logging.getLogger('logger')
+
+# Create a FileHandler to write logs to a file
+file_handler = logging.FileHandler('app.log', mode='w')
+logger.addHandler(file_handler)
 
 
 def main(args: argparse.Namespace):
     if args.download:
-        print("Starting to download databases")
+        logger.debug("Starting to download databases")
         download()
+        logger.debug("Finished downloading databases")
+
 
 
 if __name__ == '__main__':
@@ -18,8 +36,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='MeMoMe - Cool stuff.')
     # Specifying this tells the program to download all the databases
     parser.add_argument('--download', action='store_true', help='Download all required databases')
-
+    parser.add_argument('--model1', action='store', help='Path to the first model that should be merged')
+    parser.add_argument('--model2', action='store', help='Path to the second model that should be merged')
     args = parser.parse_args()
+    # Log arguments
+    logger.debug(args)
     main(args)
-
-
