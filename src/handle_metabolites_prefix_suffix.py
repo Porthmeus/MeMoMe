858330aa0bb5aebab0ed0ebd18f42138cd7cd595
Stefano_Flor <s.flor@iem.uni-kiel.de>
2023-10-27 08:13:23 -0700

    fixed circular import

--- src/handle_metabolites_prefix_suffix.py
+++ src/handle_metabolites_prefix_suffix.py
@@ -5,32 +5,29 @@ import re
 from typing import Optional
 import sys
 sys.path.append('../')
-from src.MeMoMetabolite import MeMoMetabolite
 
-def handle_metabolites_prefix_suffix(metabolites: list[MeMoMetabolite]) -> list:
+def handle_metabolites_prefix_suffix(met_id: str) -> str:
     '''
-    Parses the SBML model object, and extracts metabolite after prefix and suffix removal 
-    Parameters
+    # function to handle some specifics of the naming convention in SBML files and metabolic modeling
     ----------
-        sbml_file_url : pathlib.Path -  an sbml model file path
+        met_id : str -  the id to be manipulated
+        identifier: str - the id without prefix and suffix
     '''
-    identifiers = list()
-    for met in metabolites:
-        string = met.id
-        if "cpd" in string: # extract cpd
-            cpd_index = string.find("cpd")
-            met_id = "cpd"
-            # go to the index of 'cpd' + 3(the length) of cpd because we now expect
-            # the numerical id
-            for i in range(cpd_index + 3, len(string)):
-                # only append the digits and stop as soon as you encounter a non-digit
-                if string[i].isdigit():
-                    met_id += string[i]
-                else:
-                    break
-        else: # remove 'M_' prefix and compartment string from metabolite id, preserving content between them.
-            met_id = re.sub("^M_(.*)_[a-z]\d?$","\g<1>", string)
-            met_id = re.sub("^M_(.*)__91__[a-z]\d?__93__$","\g<1>", met_id)
-            met_id = re.sub("^M_(.*)__40__[a-z]\d?__41__$","\g<1>", met_id)
-        identifiers.append(met_id)
-    return identifiers
+
+    identifier = met_id
+    if "cpd" in met_id: # extract cpd
+        cpd_index = met_id.find("cpd")
+        identifier = "cpd"
+        # go to the index of 'cpd' + 3(the length) of cpd because we now expect
+        # the numerical id
+        for i in range(cpd_index + 3, len(met_id)):
+            # only append the digits and stop as soon as you encounter a non-digit
+            if met_id[i].isdigit():
+                identifier += met_id[i]
+            else:
+                break
+    else: # remove 'M_' prefix and compartment string from metabolite id, preserving content between them.
+        identifier = re.sub("^M_(.*)_[a-z]\d?$","\g<1>", met_id)
+        identifier = re.sub("^M_(.*)__91__[a-z]\d?__93__$","\g<1>", identifier)
+        identifier = re.sub("^M_(.*)__40__[a-z]\d?__41__$","\g<1>", identifier)
+    return identifier
