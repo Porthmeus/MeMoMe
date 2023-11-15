73a2139047bc6ddc8c4e29bd2a2ab95f30243a7d
Bram Nap <70942965+bramnap@users.noreply.github.com>
2023-11-15 10:26:51 +0000

    Update removeDuplMetsRxns.py
    Added annotations to the script
    Added the output of a specific cobramodel that is updated from the main script.

--- src/removeDuplMetsRxns.py
+++ src/removeDuplMetsRxns.py
@@ -5,7 +5,7 @@ from src.MeMoModel import *
 def update_model_duplicateMetId(memomemodel, duplcmets, metid2keep=None):
     # TODO: make it so it can take a dictionary with key being the metid2keep?
 
-    """Main script that calls on the modular functions to identify, remove and or update reactions with pre-defined
+    """Main function that calls on the modular functions to identify, remove and or update reactions with pre-defined
     duplicated metabolites
 
     Input:
@@ -27,19 +27,36 @@ def update_model_duplicateMetId(memomemodel, duplcmets, metid2keep=None):
     duplcRxn, rxn2remove, forUsrCons, rxn2update = identify_rxn_dupl_metid(memomemodel, duplcmetsm, targetid=metid2keep)
     rmv_memomemodel = remove_reaction(memomemodel, rxn2remove)
     upd_rmv_memomemodel = update_reaction(rmv_memomemodel, xn2update, metid2keep)
-    return upd_rmv_memomemodel
+    upd_rmv_cobramodel = upd_rmv_memomemodel.cobra_model
+    return upd_rmv_memomemodel, upd_rmv_cobramodel
 
 
 def identify_rxn_dupl_metid(model, duplcmets, targetid):
+    """"Function that identifies reactions associated with given metabolites and checks if they're the same reaction
+    Input:
+    model = MeMoMe model structure. Needs to have the .cobra_model field
+    duplcmets = a list of strings with identical metabolites
+    targetid = string, the metabolite ID to keep in the model
 
+    Output:
+    duplcRxn = dict, stores which reactions are the same {rxn desired ID: [rxn(s) undesired ID]}
+    rxn2remove = list, reactions that have to be removed as they are duplicates
+    forUsrCons = dict, duplicated reactions that need to be checked for bound or gpr information before removing
+                 {rxn desired ID: [rxn(s) undesired ID]}
+    rxn2update = dict, unique reactions that need to have the metabolite ID updated {rxn ID: undesired met ID}
+    """
+
+    # Initialise storage variables
     metRxn = {}
     easyReadRxnDict = {}
     altEasyReadRxnDict = {}
 
+    # Find and save in which compartments the metabolites are present
     for userMet in duplcmets:
         searchID = '^'+userMet+r'\[\w\]'
         modelMets = model.cobra_model.metabolites.query(searchID)
 
+        # Obtain the reactions associated with compartment specific metabolites
         for met in modelMets:
             rxns = model.cobra_model.metabolites.get_by_id(met.id).reactions
             for rxn in rxns:
@@ -48,11 +65,14 @@ def identify_rxn_dupl_metid(model, duplcmets, targetid):
                 else:
                     metRxn[userMet] += [rxn.id]
 
+                # For associated reactions obtain all metabolites and stoichiometries that play a role
                 rxninfo = rxn.metabolites
                 combinedMets = []
                 altcombinedMets = []
                 for metstoich in rxninfo:
                     combinedstr = metstoich.id + str(rxninfo[metstoich])
+
+                    # Account for eventual reversal of substrates and products
                     altcominedstr = metstoich.id + str(rxninfo[metstoich]*-1)
                     if userMet is not targetid:
                         combinedstr = combinedstr.replace(userMet, targetid)
@@ -62,8 +82,11 @@ def identify_rxn_dupl_metid(model, duplcmets, targetid):
                     easyReadRxnDict[rxn.id] = sorted(combinedMets)
                     altEasyReadRxnDict[rxn.id] = sorted(altcombinedMets)
 
+    # Initialse variables
     rxns2keep = []
     rxns2inspect = []
+
+    # Seperate reactions that have the desired and undesired metabolite IDs
     for key in metRxn:
         if key is targetid:
             # Make a dict with all the ids and stoichs per reactions
@@ -71,11 +94,13 @@ def identify_rxn_dupl_metid(model, duplcmets, targetid):
         else:
             rxns2inspect += metRxn[key]
 
+    # Initialise variables
     duplcRxn = {}
     rxn2remove = []
-    rxn2update = []
+    rxn2update = {}
     forUsrCons = {}
 
+    # Check if the two reactions are the same metabolite and stoichiometry wise
     for rxnIns in rxns2inspect:
         rxn1 = easyReadRxnDict[rxnIns]
         rxnalt = altEasyReadRxnDict[rxnIns]
@@ -84,6 +109,7 @@ def identify_rxn_dupl_metid(model, duplcmets, targetid):
             if rxn1 == rxn2:
                 print(rxnIns + ' is the same as ' + reactionComp)
 
+                # Check additional information on reactions to ensure no information is lost
                 rxn1ub = model.cobra_model.reactions.get_by_id(rxnIns).upper_bound
                 rxn1lb = model.cobra_model.reactions.get_by_id(rxnIns).lower_bound
                 rxn1gpr = model.cobra_model.reactions.get_by_id(rxnIns).gene_reaction_rule
@@ -97,7 +123,10 @@ def identify_rxn_dupl_metid(model, duplcmets, targetid):
                 else:
                     print(f'The bounds and/or gpr for {rxnIns} and {reactionComp} are not the same. User should check the '
                           f'reactions and make sure all information is stored in 1 reaction before removing the other')
-                    forUsrCons += [rxnIns]
+                    if reactionComp not in forUsrCons:
+                        forUsrCons[reactionComp] = [rxnIns]
+                    else:
+                        forUsrCons[reactionComp] += [rxnIns]
 
                 if reactionComp not in duplcRxn:
                     duplcRxn[reactionComp] = [rxnIns]
@@ -107,7 +136,11 @@ def identify_rxn_dupl_metid(model, duplcmets, targetid):
             elif rxnalt == rxn2:
                 print(rxnIns + ' is the same as but reversed ' + reactionComp + ' User has to double check reactions '
                                                                                 'before removing')
-                forUsrCons += [rxnIns]
+                if reactionComp not in forUsrCons:
+                    forUsrCons[reactionComp] = [rxnIns]
+                else:
+                    forUsrCons[reactionComp] += [rxnIns]
+
                 if reactionComp not in duplcRxn:
                     duplcRxn[reactionComp] = [rxnIns]
                 else:
@@ -121,6 +154,14 @@ def identify_rxn_dupl_metid(model, duplcmets, targetid):
 
 
 def remove_reaction(memome_model, reactions2remove):
+    """" Function that "removes" reactions - now only sets the bounds to zero
+    Input:
+    memome_model = MeMoMe model structure. Needs to have the .cobra_model field
+    reactions2remove = list, reaction IDs to be removed
+
+    Output:
+    memome_model = MeMoMe model structure. updated to have reactions2remove bounds set to 0
+    """
 
     for rxn in reactions2remove:
         memome_model.cobra_model.reactions.get_by_id(rxn).upper_bound = 0
@@ -129,17 +170,32 @@ def remove_reaction(memome_model, reactions2remove):
 
 
 def update_reaction(memome_model, reactions_oldMet, newmetID):
+    """" Function that updated the metabolite ID in specified reactions
+    Input:
+    memome_model = MeMoMe model structure. Needs to have the .cobra_model field
+    reactions_oldMet = dict, reactions that need to have the metabolite ID updated {rxn ID: undesired met ID}
+    newmetID = string, desired metabolite ID
+
+    Output:
+    memome_model = MeMoMe model structure. updated have specified reactions carry the desired metabolite ID
+    """
+
     for rxn in reactions_oldMet:
+        # Obtain old metabolite ID
         oldmet = reactions_oldMet[rxn]
-                      
+
+        # set the reaction object from the model for easy use
         rxn2update = memome_model.cobra_model.reactions.get_by_id(rxn)
+        # Obtain the metabolites of the reaction and the comparment and stoichiometry of the undesired metabolite ID
         totalmets = rxn2update.metabolites
         oldmetentry = [(key, value) for key, value in totalmets.items() if key.startswith(oldmet)]
         oldmet2remove = oldmetentry[0][0]
         stoich = oldmetentry[0][1]
         comp = oldmet2remove[-3:len(oldmet2remove)]
+        # Set the compartment to the desired metabolite ID
         met2add = newmetID+comp
 
+        # Remove and add the metabolites to the reactions
         rxn2update.subtract_metabolites({memome_model.cobra_model.metabolites.get_by_id(oldmet2remove): stoich})
         rxn2update.add_metabolites({memome_model.cobra_model.metabolites.get_by_id(met2add): stoich})
 
