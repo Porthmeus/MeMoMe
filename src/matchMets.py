a352d1eaee5a1966dd2592eba5ea7ef792284cb6
Porthmeus <jan.taubenheim@web.de>
2023-11-24 15:15:35 +0100

    Removed logging for rdkit warning which was cluttering the screen Improved performance of bulk inchi comparison

--- src/matchMets.py
+++ src/matchMets.py
@@ -4,16 +4,22 @@
 from src.MeMoMetabolite import MeMoMetabolite
 import pandas as pd
 from src.matchInchiRoutines import *
-from rdkit import Chem
+from rdkit import Chem, RDLogger
 from Levenshtein import ratio
 
 
-def matchMetsByInchi(inchi1:str, inchi2:str) -> tuple:
+def matchMetsByInchi(inchi1:str, inchi2:str, verbose:bool = True) -> tuple:
     ''' A combination of different matching algorithms for the Inchi-Strings, which end in a simple yes/no answer, whether the molecules are the same'''
 
+    # turn off chattiness of rdkit
+    if verbose == False:
+        RDLogger.DisableLog("rdApp.*")
+
+    # rdkit is somewhat chatty. To supress the warning:
     # check whether the molecules are differently charged, if so neutralize the charges
     m1 = Chem.MolFromInchi(inchi1)
     m2 = Chem.MolFromInchi(inchi2)
+    #try:
     charge1 = Chem.GetFormalCharge(m1)
     charge2 = Chem.GetFormalCharge(m2)
     chargeDiff = charge1 - charge2
@@ -25,15 +31,19 @@ def matchMetsByInchi(inchi1:str, inchi2:str) -> tuple:
 
     # check the fingerprints - they should be the same, if not, check at the same time, whether after H removal there is only one atom left.
     same = False
-    if compareInchiByFingerprint(inchi1,inchi2) == 1.0 or m1.GetNumAtoms() == m2.GetNumAtoms() == 1:
+    if compareInchiByFingerprint0(m1,m2, verbose = verbose) == 1.0 or m1.GetNumAtoms() == m2.GetNumAtoms() == 1:
 
         # checking the canonical InchiString
-        same = compareInchiByString(inchi1, inchi2)
+        same = compareInchiByString0(m1, m2, verbose = verbose)
         if same == False:
 
             # if that is false check if there is one of the stereoisomers the
             # same
-            same = compareInchiByStereoIsomer(inchi1,inchi2)
+            same = compareInchiByStereoIsomer0(m1,m2, verbose = verbose)
+    #except:
+    #    print("Something went wrong with the InChI comparison\nmet1: " + inchi1 + "\nmet2: " + inchi2)
+    #    same = False
+    #    chargeDiff = None
     return(same, chargeDiff)
 
 def matchMetsByDB(met1:MeMoMetabolite, met2:MeMoMetabolite) -> float:
