6392a6b003151a7439b4102949736dc0abc774de
Thomas Dost <stu203329@mail.uni-kiel.de>
2024-03-03 11:15:36 +0100

    Implemented first improvement

---
+++ tests/test_matchPerformace.py
@@ -0,0 +1,42 @@
+# Porthmeus
+# 02.08.23
+
+import unittest
+from pathlib import Path
+import cobra as cb
+import pandas as pd
+import warnings
+
+# from src.MeMoModel import MeMoModel
+from src.MeMoMetabolite import MeMoMetabolite
+from src.MeMoModel import MeMoModel
+from src.annotateBulkRoutines import *
+from src.annotateChEBI import annotateChEBI
+from src.annotateBiGG import annotateBiGG, annotateBiGG_id
+this_directory = Path(__file__).parent
+dat = this_directory.joinpath("dat")
+
+class Test_annotateBulkRoutines(unittest.TestCase):
+  def test_MeMoModelCompare2(self):
+      import time
+      
+      # Record the start time
+      start_time = time.time()
+      # test the comparison for metabolite matching
+      mod_path = dat.joinpath("e_coli_vmh.xml")
+      mod = MeMoModel.fromPath(mod_path)
+      # self comparison
+      res = mod.match(mod)
+  
+      end_time = time.time()
+      
+      # Calculate the elapsed time
+      elapsed_time = end_time - start_time
+      
+      print("Elapsed time:", elapsed_time, "seconds")
+
+
+
+
+if __name__ == '__main__':
+    pass #test_MeMoModelCompare2()
