9a3d96f6f4b2f1803a748a784918b5c3a45d4d48
Thomas Dost <stu203329@mail.uni-kiel.de>
2024-03-04 13:53:31 +0100

    Fixes more stuff

--- src/annotateChEBI.py
+++ src/annotateChEBI.py
@@ -37,7 +37,7 @@ def annotateChEBI(metabolites: list[MeMoMetabolite]) -> tuple[int, int, int]:
             if len(inchis) > 0:
                 inchi = findOptimalInchi(inchis)
                 if inchi is None:
-                    raise NotImplementedError()
+                    continue
             annotated_by_chebi = annotated_by_chebi + metabolites[i].set_inchi_string(inchi)
 
     return  annotated_by_chebi,0,0
